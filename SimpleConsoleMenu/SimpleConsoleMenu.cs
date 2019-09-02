5b922d27396e0fa394cf71283f2269a932a84bd5
Yonatan Mankovich <yonimn2000@gmail.com>
2019-09-02 10:28:10 -0400

    Added constructors to the SimpleConsoleMenu
    Added a constructor that accepts string arrays.
    Added a constructor that accepts IEnumerables.
    Added a method to add IEnumerables.

--- SimpleConsoleMenu/SimpleConsoleMenu.cs
+++ SimpleConsoleMenu/SimpleConsoleMenu.cs
@@ -1,15 +1,32 @@
-﻿
+﻿using System.Collections.Generic;
+
 namespace YonatanMankovich.SimpleConsoleMenus
 {
     public class SimpleConsoleMenu : ConsoleMenu
     {
         public SimpleConsoleMenu(string title) : base(title) { }
 
+        public SimpleConsoleMenu(string title, params string[] options) : base(title)
+        {
+            AddOptionsRange(options);
+        }
+
+        public SimpleConsoleMenu(string title, IEnumerable<string> options) : base(title)
+        {
+            AddOptionsRange(options);
+        }
+
         public SimpleConsoleMenu() : this(null) { }
 
         public void AddOption(string optionName)
         {
             menuItems.Add(optionName);
+        }
+
+        public void AddOptionsRange(IEnumerable<string> options)
+        {
+            foreach (string option in options)
+                AddOption(option);
         }
 
         public void RemoveOption(string optionName)
--- SimpleConsoleMenuTest/Program.cs
+++ SimpleConsoleMenuTest/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace YonatanMankovich.SimpleConsoleMenus.Test
 {
@@ -6,13 +7,15 @@ namespace YonatanMankovich.SimpleConsoleMenus.Test
     {
         static void Main(string[] args)
         {
-            TestSimpleConsoleMenu();
-            TestSimpleActionConsoleMenu();
+            TestSimpleConsoleMenu1();
+            TestSimpleConsoleMenu2();
+            TestSimpleConsoleMenu3();
             TestSimpleConsoleMenuNoTitle();
+            TestSimpleActionConsoleMenu();
             Console.ReadLine();
         }
 
-        private static void TestSimpleConsoleMenu()
+        private static void TestSimpleConsoleMenu1()
         {
             SimpleConsoleMenu menu = new SimpleConsoleMenu("Choose an option:");
             menu.AddOption("Option 1");
@@ -22,11 +25,26 @@ namespace YonatanMankovich.SimpleConsoleMenus.Test
             Console.WriteLine($"You selected index {menu.SelectedIndex} which is '{menu.GetSelectedItemName()}'");
         }
 
+        private static void TestSimpleConsoleMenu2()
+        {
+            SimpleConsoleMenu menu = new SimpleConsoleMenu("Choose an option:", "Option 1", "Option 2", "Option 3");
+            menu.Show();
+            Console.WriteLine($"You selected index {menu.SelectedIndex} which is '{menu.GetSelectedItemName()}'");
+        }
+
+        private static void TestSimpleConsoleMenu3()
+        {
+            IEnumerable<string> options = new List<string>() { "Option 1", "Option 2", "Option 3" };
+            SimpleConsoleMenu menu = new SimpleConsoleMenu("Choose an option:", options);
+            menu.Show();
+            Console.WriteLine($"You selected index {menu.SelectedIndex} which is '{menu.GetSelectedItemName()}'");
+        }
+
         private static void TestSimpleActionConsoleMenu()
         {
             SimpleActionConsoleMenu menu = new SimpleActionConsoleMenu("Choose an option with an action:");
             menu.AddOption("Write 'Hello!'", () => Console.WriteLine("Hello!"));
-            menu.AddOption("Show another menu", TestSimpleConsoleMenu);
+            menu.AddOption("Show another menu", TestSimpleConsoleMenu1);
             menu.AddOption("Exit", () => Environment.Exit(0));
             menu.ShowAndDoAction();
         }

316d355cf16ed3cf7853bdfb091f86d259aad183
Yonatan Mankovich <yonimn2000@gmail.com>
2023-04-08 18:44:48 -0400

    Update test .NET version.

--- SimpleConsoleMenuTest/Program.cs
+++ SimpleConsoleMenuTest/Program.cs
@@ -1,71 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading;
+﻿using YonatanMankovich.SimpleConsoleMenus;
 
-namespace YonatanMankovich.SimpleConsoleMenus.Test
+TestSimpleConsoleMenu1();
+TestSimpleConsoleMenuHide();
+TestSimpleConsoleMenu3();
+TestSimpleConsoleMenuNoTitle();
+TestSimpleActionConsoleMenu();
+TestSimpleActionConsoleMenuHide();
+
+Console.WriteLine("Press ENTER to exit . . .");
+Console.ReadLine();
+
+static void TestSimpleConsoleMenu1()
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            TestSimpleConsoleMenu1();
-            TestSimpleConsoleMenuHide();
-            TestSimpleConsoleMenu3();
-            TestSimpleConsoleMenuNoTitle();
-            TestSimpleActionConsoleMenu();
-            TestSimpleActionConsoleMenuHide();
+    SimpleConsoleMenu menu = new SimpleConsoleMenu("Choose an option:");
+    menu.AddOption("Option 1").AddOption("Option 2").AddOption("Option 3").Show();
+    Console.WriteLine($"You selected index {menu.SelectedIndex} which is '{menu.GetSelectedItemName()}'");
+}
 
-            Console.WriteLine("Press ENTER to exit . . .");
-            Console.ReadLine();
-        }
+static void TestSimpleConsoleMenuHide()
+{
+    SimpleConsoleMenu menu = new SimpleConsoleMenu("Choose an option:", "Option 1", "Option 2", "Option 3");
+    menu.Show();
+    Thread.Sleep(1000); // Sleep to show change.
+    menu.Hide();
+    Console.WriteLine($"You selected index {menu.SelectedIndex} which is '{menu.GetSelectedItemName()}'");
+}
 
-        private static void TestSimpleConsoleMenu1()
-        {
-            SimpleConsoleMenu menu = new SimpleConsoleMenu("Choose an option:");
-            menu.AddOption("Option 1").AddOption("Option 2").AddOption("Option 3").Show();
-            Console.WriteLine($"You selected index {menu.SelectedIndex} which is '{menu.GetSelectedItemName()}'");
-        }
+static void TestSimpleConsoleMenu3()
+{
+    IEnumerable<string> options = new List<string>() { "Option 1", "Option 2", "Option 3" };
+    SimpleConsoleMenu menu = new SimpleConsoleMenu("Choose an option:", options);
+    menu.Show();
+    Console.WriteLine($"You selected index {menu.SelectedIndex} which is '{menu.GetSelectedItemName()}'");
+}
 
-        private static void TestSimpleConsoleMenuHide()
-        {
-            SimpleConsoleMenu menu = new SimpleConsoleMenu("Choose an option:", "Option 1", "Option 2", "Option 3");
-            menu.Show();
-            Thread.Sleep(1000); // Sleep to show change.
-            menu.Hide();
-            Console.WriteLine($"You selected index {menu.SelectedIndex} which is '{menu.GetSelectedItemName()}'");
-        }
+static void TestSimpleActionConsoleMenu()
+{
+    SimpleActionConsoleMenu menu = new SimpleActionConsoleMenu("Choose an option with an action:");
+    menu.AddOption("Write 'Hello!'", () => Console.WriteLine("Hello!"))
+        .AddOption("Show another menu", TestSimpleConsoleMenu1)
+        .AddOption("Exit", () => Environment.Exit(0))
+        .ShowAndDoAction();
+}
 
-        private static void TestSimpleConsoleMenu3()
-        {
-            IEnumerable<string> options = new List<string>() { "Option 1", "Option 2", "Option 3" };
-            SimpleConsoleMenu menu = new SimpleConsoleMenu("Choose an option:", options);
-            menu.Show();
-            Console.WriteLine($"You selected index {menu.SelectedIndex} which is '{menu.GetSelectedItemName()}'");
-        }
+static void TestSimpleActionConsoleMenuHide()
+{
+    SimpleActionConsoleMenu menu = new SimpleActionConsoleMenu("Choose an option with an action:");
+    menu.AddOption("Write 'Hello!'", () => Console.WriteLine("Hello!"))
+        .AddOption("Show another menu", TestSimpleConsoleMenu1)
+        .AddOption("Exit", () => Environment.Exit(0))
+        .ShowHideAndDoAction();
+}
 
-        private static void TestSimpleActionConsoleMenu()
-        {
-            SimpleActionConsoleMenu menu = new SimpleActionConsoleMenu("Choose an option with an action:");
-            menu.AddOption("Write 'Hello!'", () => Console.WriteLine("Hello!"))
-                .AddOption("Show another menu", TestSimpleConsoleMenu1)
-                .AddOption("Exit", () => Environment.Exit(0))
-                .ShowAndDoAction();
-        }
-
-        private static void TestSimpleActionConsoleMenuHide()
-        {
-            SimpleActionConsoleMenu menu = new SimpleActionConsoleMenu("Choose an option with an action:");
-            menu.AddOption("Write 'Hello!'", () => Console.WriteLine("Hello!"))
-                .AddOption("Show another menu", TestSimpleConsoleMenu1)
-                .AddOption("Exit", () => Environment.Exit(0))
-                .ShowHideAndDoAction();
-        }
-
-        private static void TestSimpleConsoleMenuNoTitle()
-        {
-            SimpleConsoleMenu menu = new SimpleConsoleMenu();
-            menu.AddOption("Option 1").AddOption("Option 2").AddOption("Option 3").Show();
-            Console.WriteLine($"You selected index {menu.SelectedIndex} which is '{menu.GetSelectedItemName()}'");
-        }
-    }
+static void TestSimpleConsoleMenuNoTitle()
+{
+    SimpleConsoleMenu menu = new SimpleConsoleMenu();
+    menu.AddOption("Option 1").AddOption("Option 2").AddOption("Option 3").Show();
+    Console.WriteLine($"You selected index {menu.SelectedIndex} which is '{menu.GetSelectedItemName()}'");
 }
